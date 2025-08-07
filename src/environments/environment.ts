73879db4b5ecfc17a936d4b6867282bf041400e8
venkadesh1999-M <messivenkadesh135@gmail.com>
2025-08-07 22:03:47 +0530

    Update environment.ts
    Some changes in Environment File

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,6 @@
 export const environment = {
     production: false,
-    // firebaseConfig: {
-    //     apiKey: "AIzaSyDJ3Web-NUe2L0k_joWW_5Jpje9jSZ6Rbo",
-    //     authDomain: "merlux-dev-40b0d.firebaseapp.com",
-    //     databaseURL: "https://merlux-dev-40b0d-default-rtdb.firebaseio.com",
-    //     projectId: "merlux-dev-40b0d",
-    //     storageBucket: "merlux-dev-40b0d.appspot.com",
-    //     messagingSenderId: "602646222053",
-    //     appId: "1:602646222053:web:aa1d890da61a168571700c",
-    //     measurementId: "G-CFHLP5ZSG5"
-    // }
-
-    firebaseConfig :{
+  firebaseConfig :{
   apiKey: "AIzaSyBTcnRjaueVKYnAPXkBowLXKmQL30Y4D3k",
   authDomain: "employee-app-e4f23.firebaseapp.com",
   projectId: "employee-app-e4f23",
